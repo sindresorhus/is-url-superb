deed661c5d319250523541592965e42da449997a
Kevin Martensson <kevinmartensson@gmail.com>
2015-06-22 18:24:39 +0200

    Bump `url-regex`

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ var test = require('ava');
 var isUrl = require('./');
 
 test(function (t) {
-	t.assert(isUrl('todomvc.com'));
+	t.assert(isUrl('//todomvc.com'));
 	t.assert(isUrl('http://todomvc.com'));
 	t.assert(!isUrl('abc http://todomvc.com'));
 	t.end();
