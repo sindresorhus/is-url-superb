0693dc88dce41beda2cd8dcfac72c3122bb6e9d4
Brandon Smith <4730164+brandon93s@users.noreply.github.com>
2020-11-28 07:24:01 -0500

    Return `false` when URL contains unescaped spaces

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ module.exports = string => {
 		throw new TypeError('Expected a string');
 	}
 
+	string = string.trim();
+	if (string.includes(' ')) {
+		return false;
+	}
+
 	try {
 		new URL(string); // eslint-disable-line no-new
 		return true;
--- test.js
+++ test.js
@@ -3,5 +3,8 @@ import isUrl from '.';
 
 test('main', t => {
 	t.true(isUrl('https://sindresorhus.com'));
+	t.true(isUrl('  https://sindresorhus.com  '));
 	t.false(isUrl('abc https://sindresorhus.com'));
+	t.false(isUrl('https://sindresorhus.com abc'));
+	t.false(isUrl('https://sindresorhus.com/abc def'));
 });
